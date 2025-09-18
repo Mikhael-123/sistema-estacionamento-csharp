e8b8e11e502e09f8e6b1471fd1fd3bf27e761a59
Mikhael-123 <paulomiguelbentes@gmail.com>
2025-09-18 08:20:34 -0400

    Projeto Inicial

---
+++ DesafioFundamentosDIO.Console/Models/Estacionamento.cs
@@ -0,0 +1,42 @@
+﻿namespace DesafioFundamentosDIOConsole.Models;
+
+public class Estacionamento
+{
+  private decimal valorInicial = 0;
+  private decimal valorPorHora = 0;
+  Utils utils = new Utils(0, 0);
+  private List<string> veiculos = new List<string>();
+
+  public Estacionamento(decimal valorInicial, decimal valorPorHora)
+  {
+    this.valorInicial = valorInicial;
+    this.valorPorHora = valorPorHora;
+    this.utils = new Utils(valorInicial, valorPorHora);
+  }
+
+  public void CadastrarVeiculo()
+  {
+    Console.WriteLine("");
+    Console.WriteLine("Escreva a placa do veículo:");
+    string placa = Convert.ToString(Console.ReadLine());
+    this.veiculos.Add(placa);
+    Console.WriteLine("");
+    Console.WriteLine("Veículo estacionado com sucesso, pressione qualquer tecla para continuar");
+    Console.Read();
+    this.utils.ComeçarPrograma();
+  }
+  public void RemoverVeiculo() { }
+  public void ListarVeiculos()
+  {
+    Console.WriteLine("");
+    Console.WriteLine("Veículos estacionados:");
+    foreach (string placa in this.veiculos)
+    {
+      Console.WriteLine(placa);
+    }
+    Console.WriteLine("");
+    Console.WriteLine("Pressione qualquer tecla para continuar");
+    Console.Read();
+    this.utils.ComeçarPrograma();
+  }
+}
---
+++ DesafioFundamentosDIO.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿using DesafioFundamentosDIOConsole.Models;
+
+Console.WriteLine("Bem-Vindo ao Estacionamento!");
+Console.WriteLine("Digite o valor inicial cobrado por estacionar:");
+decimal valorInicial = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Digite o valor cobrado por hora de estacionamento:");
+decimal valorPorHora = Convert.ToDecimal(Console.ReadLine());
+
+Utils utils = new Utils(valorInicial, valorPorHora);
+utils.ComeçarPrograma();
